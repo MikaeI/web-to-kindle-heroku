d313e331a0b87764bd8f05bf61503f67c8563a26
Mikael Åsbjørnsson-Stensland <mikael@grensesnitt.no>
2022-05-15 23:03:27 +0200

    Fixes

--- index.js
+++ index.js
@@ -1,44 +1,58 @@
-const express = require('express');
-const path = require('path');
-const puppeteer = require('puppeteer');
-const execFile = require('child_process').execFile;
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const puppeteer = require("puppeteer");
+const execFile = require("child_process").execFile;
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', async (req, res) => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", async (req, res) => {
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
     await page.setViewport({ width: 1080, height: 1440 });
-    await page.goto(process.env.SCREENSHOT_URL || 'https://darksky.net/details/40.7127,-74.0059/2021-1-6/us12/en');
-    await page.waitFor(5000);
-    console.log("Howdy!");
+    await page.goto(
+      process.env.SCREENSHOT_URL || "https://mikaei.github.io/info-screen/",
+      { waitUntil: "networkidle0", timeout: 15000 }
+    );
+    await page.waitForTimeout(1000);
     await page.screenshot({
-      path: '/tmp/screenshot.png',
+      path: "/tmp/screenshot.png",
     });
 
     await browser.close();
 
-    await convert('/tmp/screenshot.png');
-    screenshot = fs.readFileSync('/tmp/screenshot.png');
+    await convert("/tmp/screenshot.png");
+    screenshot = fs.readFileSync("/tmp/screenshot.png");
 
     res.writeHead(200, {
-      'Content-Type': 'image/png',
-      'Content-Length': screenshot.length,
+      "Content-Type": "image/png",
+      "Content-Length": screenshot.length,
     });
     return res.end(screenshot);
   })
-  .listen(PORT, () => console.log(`Listening intently on ${PORT}`));
-
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 function convert(filename) {
   return new Promise((resolve, reject) => {
-    const args = [filename, '-gravity', 'center', '-extent', '600x800', '-colorspace', 'gray', '-depth', '8', filename];
-    execFile('convert', args, (error, stdout, stderr) => {
+    const args = [
+      filename,
+      "-gravity",
+      "center",
+      "-extent",
+      "1080x1440",
+      "-colorspace",
+      "gray",
+      "-depth",
+      "8",
+      filename,
+    ];
+    execFile("convert", args, (error, stdout, stderr) => {
       if (error) {
         console.error({ error, stdout, stderr });
         reject();
